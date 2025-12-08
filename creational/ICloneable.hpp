443d3de880e8236349860eccf7c46dcca6d34657
Peter Jaglom <peter@peterjaglom.com>
2025-12-07 21:25:02 -0800

    ICloneable update on call with Bob
    - renamed template parameter class to T from Derived
    - added pointer to memor_resource in ICloneable

--- creational/ICloneable.hpp
+++ creational/ICloneable.hpp
@@ -4,9 +4,9 @@
 #include <type_traits>
 
 
-template<typename Derived> 
+template<typename T> 
 struct ICloneable{
-    friend Derived;
+    friend T;
 
     struct PmrDeleter {
         using DestroyFn = void(*)(ICloneable*, std::pmr::memory_resource*) noexcept;
@@ -31,30 +31,33 @@ struct ICloneable{
     virtual ~ICloneable() = default;
 
 private:
+
+    std::pmr::memory_resource* mr = nullptr; // TODO: Change this name to avoid confusion with the memory resource parameter of the clone method?
     // private, default constructor: no data members, private + friend enforces proper CRTP
     ICloneable() = default;
     // private, default copy constructor: no data members to copy, private + friend prevents 
     // object slicing while allowing derived classes to be copied normally
     ICloneable(const ICloneable&) = default;
 
-    // Static destruction logic specific to Derived, but with a uniform signature.
+    // Static destruction logic specific to T, but with a uniform signature.
     static void destroy_impl(ICloneable* base, std::pmr::memory_resource* mr) noexcept {
-        auto* d = static_cast<Derived*>(base);
-        d->~Derived();
-        mr->deallocate(d, sizeof(Derived), alignof(Derived));
+        auto* d = static_cast<T*>(base);
+        d->~T();
+        mr->deallocate(d, sizeof(T), alignof(T));
     }
 
 public:
 
     // Clone into the given memory resource.
     // Contract: the caller guarantees that `mr` outlives the clone.
-    virtual std::unique_ptr<Derived, PmrDeleter> clone(std::pmr::memory_resource* mr = nullptr) const {
-        using D = Derived;
+    virtual std::unique_ptr<T, PmrDeleter> clone(std::pmr::memory_resource* mr = nullptr) const {
+        using D = T;
         auto const& self = static_cast<D const&>(*this);
+        // instantiate a polymorphic allocator using the memory resource for the type of T
 
         if (!mr) {
             // Plain new/delete path
-            return std::unique_ptr<Derived, PmrDeleter>(new D(self), PmrDeleter{}); // default-constructed deleter => delete
+            return std::unique_ptr<T, PmrDeleter>(new D(self), PmrDeleter{}); // default-constructed deleter => delete
         }
 
         // Allocate object storage from the given memory_resource
@@ -62,7 +65,7 @@ public:
         try {
             // NOTE: D must have: D(D const&, std::pmr::memory_resource*)
             D* p = new (mem) D(self, mr);
-            return std::unique_ptr<Derived, PmrDeleter>(p, PmrDeleter{ mr, &destroy_impl });
+            return std::unique_ptr<T, PmrDeleter>(p, PmrDeleter{ mr, &destroy_impl });
         } catch (...) {
             mr->deallocate(mem, sizeof(D), alignof(D));
             throw;

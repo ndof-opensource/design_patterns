1956bb9daaae2aca60b2296290166a881d323ee9
Peter Jaglom <peter@peterjaglom.com>
2026-01-04 17:10:54 -0800

    Updated ICloneable per 12/14/2025 Notes from Bob
    - Deleted move assignment operator
    - Deleted copy assignment operator
    - Templated ICloneable on allocator, with `std::pmr::polymorphic_allocator` as default
    - Made allocator holder [[no_unique_address]]

--- creational/ICloneable.hpp
+++ creational/ICloneable.hpp
@@ -4,15 +4,16 @@
 #include <type_traits>
 
 
-template<typename T> // TODO: Add allocator as template parameter
+template<typename T, typename Alloc = std::pmr::polymorphic_allocator<T>>
 struct ICloneable {
     friend T;
 
-    // Allocator-aware deleter using allocator_traits
-    template<typename Alloc>
+    // Allocator-aware deleter using allocator_traits.
+    // Templated on allocator type (A) to support rebinding between related types.
+    template<typename A>
     struct AllocDeleter {
-        using allocator_type = Alloc;
-        using alloc_traits = std::allocator_traits<Alloc>;
+        using allocator_type = A;
+        using alloc_traits = std::allocator_traits<A>;
 
         allocator_type alloc{};
 
@@ -22,8 +23,8 @@ struct ICloneable {
         // Enable conversion from AllocDeleter<rebind> for base/derived pointer conversions
         template<typename OtherAlloc>
         requires std::is_same_v<
-            typename std::allocator_traits<OtherAlloc>::template rebind_alloc<T>,
-            Alloc
+            typename std::allocator_traits<OtherAlloc>::template rebind_alloc<typename alloc_traits::value_type>,
+            A
         >
         AllocDeleter(const AllocDeleter<OtherAlloc>& other) 
             : alloc(other.alloc) {}
@@ -37,90 +38,105 @@ struct ICloneable {
         }
     };
 
-    // Default allocator and deleter types for convenience
-    using DefaultAlloc = std::pmr::polymorphic_allocator<T>;
-    using PmrDeleter = AllocDeleter<DefaultAlloc>;
+    // Convenient alias for the primary allocator's deleter
+    using Deleter = AllocDeleter<Alloc>;
 
     virtual ~ICloneable() = default;
 
 private:
-    std::pmr::memory_resource* mem_res = nullptr; // TODO: Change all mem_res to allocator - no long hold a memory resource
-    // private, default constructor: no data members, private + friend enforces proper CRTP
+    // Store the allocator for default cloning. [[no_unique_address]] allows
+    // stateless allocators (like std::allocator) to take zero space.
+    [[no_unique_address]] Alloc allocator_{};
+    
+    // private, default constructor
     ICloneable() = default;
-    // private, default copy constructor: no data members to copy, private + friend prevents 
-    // object slicing while allowing derived classes to be copied normally
+    // Constructor to set the allocator
+    explicit ICloneable(Alloc a) : allocator_(std::move(a)) {}
+    // Copy constructor: copies allocator
     ICloneable(const ICloneable&) = default;
+    // Move constructor: moves allocator
+    ICloneable(ICloneable&&) = default;
+    // Copy and move assignment deleted
+    ICloneable& operator=(const ICloneable&) = delete;
+    ICloneable& operator=(ICloneable&&) = delete;
 
 public:
 
-    // Clone using a polymorphic_allocator.
+    // Clone using an allocator.
     // Uses select_on_container_copy_construction for proper allocator propagation.
-    // Contract: the allocator's memory_resource must outlive the clone.
+    // Delegates to virtual do_clone() for polymorphic construction.
+    // Contract: the allocator must outlive the clone.
     template<typename U>
     requires std::is_base_of_v<T, U>
-    std::unique_ptr<std::remove_reference_t<U>, AllocDeleter<std::pmr::polymorphic_allocator<U>>> 
-    clone(this U const& self, std::pmr::polymorphic_allocator<U> alloc = {}) {
-        using Alloc = std::pmr::polymorphic_allocator<U>;
+    std::unique_ptr<U, AllocDeleter<typename std::allocator_traits<Alloc>::template rebind_alloc<U>>> 
+    clone(this U const& self, Alloc alloc = Alloc{}) {
         using alloc_traits = std::allocator_traits<Alloc>;
-        using Deleter = AllocDeleter<Alloc>;
+        using ReboundAlloc = typename alloc_traits::template rebind_alloc<U>;
+        using ReboundDeleter = AllocDeleter<ReboundAlloc>;
 
         // Use select_on_container_copy_construction for proper propagation semantics.
+        // TODO: Discussion point. We need to decide what we want for this logic.
+        // As is, this will first select the allocator to use for container copy construction,
+        // then, if that allocator is the default resource, we use the object's stored allocator.
+        // But what if the user passes a non-default resource but the select_on_container_copy_construction is the default?
+        // Then we will use the stored allocator, but maybe we should have used the allocator the user passed instead?
+        // Reminder for Peter: both std::allocator and std::pmr::polymorphic_allocator use default-constructed for select_on...
         Alloc new_alloc = alloc_traits::select_on_container_copy_construction(alloc);
 
-        // If using default resource and the object has a stored resource, use it
-        if (new_alloc.resource() == std::pmr::get_default_resource() && self.mem_res) {
-            new_alloc = Alloc{self.mem_res};
+        // If passed a default-constructed allocator, try to use the object's stored allocator.
+        // This check is specific to polymorphic_allocator which has a resource() method.
+        if constexpr (requires { new_alloc.resource(); }) {
+            if (new_alloc.resource() == std::pmr::get_default_resource()) {
+                new_alloc = self.allocator_;
+            }
         }
 
-        // Delegate to virtual do_clone
-        std::unique_ptr<T, PmrDeleter> r = self.do_clone(new_alloc.resource());
+        // Delegate to virtual do_clone for polymorphic construction
+        std::unique_ptr<T, Deleter> r = self.do_clone(new_alloc);
+        
+        // Transfer ownership with rebound deleter
+        Deleter old_deleter = r.get_deleter();
         T* obj = r.release();
         
-        return std::unique_ptr<std::remove_reference_t<U>, Deleter>(
+        // Rebind the allocator from T to U
+        ReboundAlloc rebound_alloc(old_deleter.alloc);
+        
+        return std::unique_ptr<U, ReboundDeleter>(
             static_cast<U*>(obj), 
-            Deleter(new_alloc)
+            ReboundDeleter(rebound_alloc)
         );
     }
 
 protected:
     /* TODO: Discussion point.
-    *  this relies on each derived class properly overriding do_clone() to construct its 
-    * actual type. If a class in the middle of the hierarchy doesn't override it, the 
-    * downcast from T* to U* could be unsafe (e.g., if Poodle inherits from Dog but only 
-    * Dog::do_clone() exists, it would construct a Dog but cast to Poodle*).
+    *  This relies on each derived class properly overriding do_clone() to construct its 
+    *  actual type. If a class in the middle of the hierarchy doesn't override it, the 
+    *  downcast from T* to U* could be unsafe (e.g., if Poodle inherits from Dog but only 
+    *  Dog::do_clone() exists, it would construct a Dog but cast to Poodle*).
     * 
-    * We could enforce this in one of at least two ways:
-    * 1. Runtime check in clone() using dynamic_cast.
-    * 2. Made do_clone() pure virtual and provide a helper so derived classes must override do_clone().
-    * 
+    *  We could enforce this in one of at least two ways:
+    *  1. Runtime check in clone() using dynamic_cast.
+    *  2. Make do_clone() pure virtual and provide a helper so derived classes must override.
+    *
+    *  Example override in derived class Dog:
+    *    std::unique_ptr<Animal, Deleter> do_clone(Alloc alloc) const override {
+    *        using DogAlloc = typename std::allocator_traits<Alloc>::template rebind_alloc<Dog>;
+    *        using dog_traits = std::allocator_traits<DogAlloc>;
+    *        DogAlloc dog_alloc(alloc);
+    *        Dog* ptr = dog_traits::allocate(dog_alloc, 1);
+    *        dog_traits::construct(dog_alloc, ptr, *this);
+    *        return std::unique_ptr<Animal, Deleter>(ptr, Deleter(dog_alloc));
+    *    }
     */
-    virtual std::unique_ptr<T, PmrDeleter> do_clone(std::pmr::memory_resource* mr = nullptr) const {
-        using D = T;
-        using Alloc = std::pmr::polymorphic_allocator<D>;
+    virtual std::unique_ptr<T, Deleter> do_clone(Alloc alloc) const {
         using alloc_traits = std::allocator_traits<Alloc>;
 
-        auto const& self = static_cast<D const&>(*this);
+        auto const& self = static_cast<T const&>(*this);
         
-        if (!mr) {
-            // Plain new/delete path - use default allocator
-            Alloc alloc{};
-            D* ptr = alloc_traits::allocate(alloc, 1);
-            try {
-                alloc_traits::construct(alloc, ptr, self);
-                return std::unique_ptr<T, PmrDeleter>(ptr, PmrDeleter(alloc));
-            } catch (...) {
-                alloc_traits::deallocate(alloc, ptr, 1);
-                throw;
-            }
-        }
-
-        // PMR path - use provided memory resource
-        Alloc alloc{mr};
-        D* ptr = alloc_traits::allocate(alloc, 1);
+        T* ptr = alloc_traits::allocate(alloc, 1);
         try {
-            // Use allocator_traits::construct which may use allocator-aware construction
             alloc_traits::construct(alloc, ptr, self);
-            return std::unique_ptr<T, PmrDeleter>(ptr, PmrDeleter(alloc));
+            return std::unique_ptr<T, Deleter>(ptr, Deleter(alloc));
         } catch (...) {
             alloc_traits::deallocate(alloc, ptr, 1);
             throw;
